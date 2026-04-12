c915d615df266d9c9b02ba33f92d818d5ff1a3cc
the.likhitjain@gmail.com <st186538@stud.uni-stuttgart.de>
2026-04-12 02:10:47 +0200

    added main

---
+++ main.py
@@ -0,0 +1,19 @@
+import threading
+import time
+import subprocess
+import uvicorn
+import requests
+
+# Start API
+threading.Thread(target=uvicorn.run, kwargs={"app": "src.app.api.root:app"}, daemon=True).start()
+
+# Wait for API
+while True:
+    try:
+        requests.get("http://localhost:8000")
+        break
+    except requests.ConnectionError:
+        time.sleep(0.5)
+
+# Start Streamlit
+subprocess.Popen(["python", "-m", "streamlit", "run", "src/app/ui/Chat.py"]).wait()
